4c309365b0c8f6c5ad7f89b8217e1798cd6ed3c1
Abdielitis67 <meirdanz4@gmail.com>
2026-04-06 17:41:10 +0700

    first commit

---
+++ ArStru1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+// Nama Repo : ArraaydanStruct_0209
+// Minimal commit : 15 
+
+int main() {
+    // deklarasi dengan inisialisasi
+    int aku[5] = {10, 20, 30, 40, 50};
+    // deklasrasi array tanpa inisialisasi
+    int kamu[5];
+    // tampilkan data(membaca data array)
+    cout << "Data pada index 1 = " << aku[1] << endl;
+    // ganti index ke 1(menulis data array)
+    aku[1] = 200;
+    cout << endl;
+    cout << "Data pada index 1= " << aku[1] << endl;
+    // mengisi satu per satu
+    cout << "Data index 0 = ";
+    cin >> kamu[0];
+    cout << "Data index 1 = ";
+    cin >> kamu[1];
+    cout << "Data index 2 = ";
+    cin >> kamu[2];
+    cout << "Data index 3 = ";
+    cin >> kamu[3];
+    cout << "Data index 4 = ";
+    cin >> kamu[4];
+    cout << endl;
+    // Menampilkan satu per satu
+    cout << "Data Pertama = " << kamu[0] << endl;
+    cout << "Data kedua = " << kamu[1] << endl;
+    cout << "Data ketiga = " << kamu[2] << endl;
+    cout << "Data keempat = " << kamu[3] << endl;
+    cout << "Data kelima = " << kamu[4] << endl;
+    cout << endl;
+
+    // deklarasi array dengan perulangan
+    for (int i = 0; i <= 4; i++) 
+    {
+        cout << "Data index ke- " << i << " = ";
+        cin >> kamu[i];
+    }
+    cout << endl;
+    // Menampilkan dengan menggunakan perulangan
+    for (int i = 0; i <= 4; i++) 
+    {
+        cout << "Data index ke- " << i << " = " << kamu[i] << endl;
+    }
+}
